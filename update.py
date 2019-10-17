ac549a1a762e53ad28293e869e7fc55d434a5ccc
Logan <wolvie2013@gmail.com>
2019-10-17 17:29:46 -0400

    Simplifying update.py

--- update.py
+++ update.py
@@ -1,12 +1,8 @@
 import subprocess, sys, time
 print("Starting update script")
-try:
-    subprocess.run('git pull',shell=True)
-    subprocess.run('git checkout origin/master .',shell=True)
-    print("Update Complete! Restarting!")
-    time.sleep(3)
-    subprocess.run('cls',shell=True)
-    subprocess.run(['python','start.py'],shell=True)
-except:
-    print("Update failed. Try downloading manually. :c")
-sys.exit()
+subprocess.run('git pull',shell=True)
+subprocess.run('git checkout origin/master .',shell=True)
+print("Update Complete! Restarting!")
+time.sleep(3)
+subprocess.run('cls',shell=True)
+subprocess.run(['python','start.py'],shell=True)
