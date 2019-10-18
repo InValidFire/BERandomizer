192fd0cb2f7ccaf0cb3f59eccad527ee64af7215
Logan <wolvie2013@gmail.com>
2019-10-18 02:23:24 -0400

    Undo

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 import subprocess, sys, time
 print("Starting update script")
 subprocess.run('git checkout origin/master .',shell=True)
-subprocess.run('git reset --hard origin/master .',shell=True)
 subprocess.run('git pull',shell=True)
 print("Update Complete! Restarting!")
 time.sleep(3)
