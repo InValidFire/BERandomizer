154488ea95e820400cbfb31dd777f2e83bf3bacb
Logan <wolvie2013@gmail.com>
2019-10-18 02:22:34 -0400

    Update modification

--- update.py
+++ update.py
@@ -1,7 +1,8 @@
 import subprocess, sys, time
 print("Starting update script")
-subprocess.run('git pull',shell=True)
 subprocess.run('git checkout origin/master .',shell=True)
+subprocess.run('git reset --hard origin/master .',shell=True)
+subprocess.run('git pull',shell=True)
 print("Update Complete! Restarting!")
 time.sleep(3)
 subprocess.run('cls',shell=True)
